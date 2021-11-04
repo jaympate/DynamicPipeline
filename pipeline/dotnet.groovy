b3078de6906955b523cc9cb7065c89398f5e51ba
jaympate <91273133+jaympate@users.noreply.github.com>
2021-11-04 15:22:59 -0400

    Update dotnet.groovy

--- pipeline/dotnet.groovy
+++ pipeline/dotnet.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Code Check Out') {
             steps {
-                git branch: 'main', credentialsId: env.Credential_ID, url: '${APP_URL}'
+                git branch: 'main', credentialsId: '${Credential_ID}, url: '${APP_URL}'
                 echo("${APP_URL} Repository was successfully cloned.")
                 echo("${BUILD_NAME} for ${APP_URL}")
                 }
